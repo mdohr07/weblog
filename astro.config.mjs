40b87d6ce8fb486b8be3bf6879ffa8e6d8ce6c3c
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-09-16 11:14:58 +0200

    edited config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-minimal-starter.netlify.app/",
+  site: "https://mdohr.space",
   integrations: [sitemap()]
 });
