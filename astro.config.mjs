5affb876179264fd46156dd20f3b74b29840df3e
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-09-17 11:07:34 +0200

    edited astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,3 +8,14 @@ export default defineConfig({
   site: "https://mdohr.space",
   integrations: [sitemap()]
 });
+
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['date-fns'],
+    },
+  },
+});
