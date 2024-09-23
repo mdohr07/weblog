07edb81291447e3373795ac5335adfced8731cf0
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-09-23 11:11:08 +0200

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,15 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
+  // Blogposts aus dem richtigen Verzeichnis laden
+  const blogPosts = import.meta.glob('./blog/*.{md,mdx}');
+
+  console.log("Gefundene Blogposts: ", blogPosts); // DEBUG: Zeigt die geladenen Blogposts an
+  
   return rss({
     title: 'mdohr’s weblog',
     description: 'A personal blog about web, coding and other things that interest me',
     site: context.site, // Stellt sicher, dass der `site`-Wert von der Config genutzt wird
-    items: await pagesGlobToRssItems(
-      import.meta.glob('./blog/*.{md,mdx}') // Stelle sicher, dass der Pfad relativ zu `src/pages` ist
-    ),
+    items: await pagesGlobToRssItems(blogPosts),
   });
 }
