b74b57724031aa670857f7f8493fffaf6a33f6ae
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-09-23 11:07:19 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
+import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import rss from '@astrojs/rss';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mdohr.space",
-  integrations: [sitemap()]
+  site: "https://mdohr.space", // Die Basis-URL deiner Website
+  integrations: [
+    sitemap(), // Sitemap-Integration
+    rss()      // RSS-Integration hinzufügen
+  ],
 });
