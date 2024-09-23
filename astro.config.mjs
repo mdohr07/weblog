8764233da839133a5931217d70ee93d9b40db834
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-09-23 11:04:57 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
 export default defineConfig({
-    site: 'https://mdohr.space', 
-  });
+  site: "https://mdohr.space",
+  integrations: [sitemap()]
+});
