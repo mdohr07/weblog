4fccacf5851f77a6e86d053cef17ea5f753e11d1
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-09-23 10:33:58 +0200

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,20 +1,11 @@
+import { defineConfig } from 'astro/config';
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
 
-export async function GET(context) {
-  const blog = await getCollection('blog');
-  return rss({
-    title: 'mdohr\'s weblog',
-    description: 'My personal blog about IT, web and other stuff that interests me',
-    site: context.site,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      customData: post.data.customData,
-      // Berechne den RSS link vom Beitrags-`slug`
-      // In diesem Beispiel wird davon ausgegangen, dass alle Beiträge als `/blog/[slug]`-Routen gerendert werden
-      link: `/blog/${post.slug}/`,
-    })),
-  });
-}
+export default defineConfig({
+  integrations: [rss()],
+  build: {
+    rollupOptions: {
+      external: ['astro:content'],
+    },
+  },
+});
