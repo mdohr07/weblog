2d1a6f33f45a66732faf419ecb0d14d4c6e54a25
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2024-09-23 10:49:30 +0200

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,18 +1,12 @@
-import rss from '@astrojs/rss';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-export function GET(context) {
+export async function GET(context) {
   return rss({
-    // `<title>` field in output xml
     title: 'mdohr’s weblog',
-    // `<description>` field in output xml
     description: 'A personal blog about web, coding and other things that interest me',
-    // Pull in your project "site" from the endpoint context
-    // https://docs.astro.build/en/reference/api-reference/#contextsite
     site: context.site,
-    // Array of `<item>`s in output xml
-    // See "Generating items" section for examples using content collections and glob imports
-    items: [],
-    // (optional) inject custom xml
-    customData: `<language>en-us</language>`,
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./pages/blog/*.{md,mdx}'),
+    ),
   });
 }
